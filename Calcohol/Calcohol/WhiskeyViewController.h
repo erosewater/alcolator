add4f2b3529691875b5100dadada1cb4f8d9282c
David Krasnove <krasnove@mac.com>
2014-11-22 12:22:33 -0800

    Wrote a custom view controller

---
+++ Calcohol/Calcohol/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Calcohol
+//
+//  Created by dbk-dev on 11/20/14.
+//  Copyright (c) 2014 David Krasnove. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
